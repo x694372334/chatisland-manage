99f6946813501a24c0479782048f1b437027fe97
jinmingyu <jinmingyu>
2023-11-10 10:18:19 +0800

    bug修改 以及 插件整合

--- src/views/system/graphical/graphicalTwo.vue
+++ src/views/system/graphical/graphicalTwo.vue
@@ -144,139 +144,139 @@ export default {
       // 通过 $ref 进行挂载
       let myChart = this.$echarts.init(this.$refs.barChart);
       // let myChart1 = this.$echarts.init(this.$refs.barChart);
-      let option = {
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'shadow'
-          }
-        },
-        legend: {},
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: [
-          {
-            type: 'category',
-            data: ['china', 'usa', 'canada', 'germany', 'britain', 'russia', 'france']
-          }
-        ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
-        series: [
-          {
-            name: 'Direct',
-            type: 'bar',
-            emphasis: {
-              focus: 'series'
-            },
-            data: [320, 332, 301, 334, 390, 330, 320]
-          },
-          {
-            name: 'Email',
-            type: 'bar',
-            stack: 'Ad',
-            emphasis: {
-              focus: 'series'
-            },
-            data: [120, 132, 101, 134, 90, 230, 210]
-          },
-          {
-            name: 'Union Ads',
-            type: 'bar',
-            stack: 'Ad',
-            emphasis: {
-              focus: 'series'
-            },
-            data: [220, 182, 191, 234, 290, 330, 310]
-          },
-          {
-            name: 'Video Ads',
-            type: 'bar',
-            stack: 'Ad',
-            emphasis: {
-              focus: 'series'
-            },
-            data: [150, 232, 201, 154, 190, 330, 410]
-          },
-          {
-            name: 'Search Engine',
-            type: 'bar',
-            data: [862, 1018, 964, 1026, 1679, 1600, 1570],
-            emphasis: {
-              focus: 'series'
-            },
-            markLine: {
-              lineStyle: {
-                type: 'dashed'
-              },
-              data: [[{type: 'min'}, {type: 'max'}]]
-            }
-          },
-          {
-            name: 'Baidu',
-            type: 'bar',
-            barWidth: 5,
-            stack: 'Search Engine',
-            emphasis: {
-              focus: 'series'
-            },
-            data: [620, 732, 701, 734, 1090, 1130, 1120]
-          },
-          {
-            name: 'Google',
-            type: 'bar',
-            stack: 'Search Engine',
-            emphasis: {
-              focus: 'series'
-            },
-            data: [120, 132, 101, 134, 290, 230, 220]
-          },
-          {
-            name: 'Bing',
-            type: 'bar',
-            stack: 'Search Engine',
-            emphasis: {
-              focus: 'series'
-            },
-            data: [60, 72, 71, 74, 190, 130, 110]
-          },
-          {
-            name: 'Others',
-            type: 'bar',
-            stack: 'Search Engine',
-            emphasis: {
-              focus: 'series'
-            },
-            data: [62, 82, 91, 84, 109, 110, 120]
-          }
-        ]
-      };
-      // let option1 = {
-      //   xAxis: {
-      //     type: "category",
-      //     data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      // let option = {
+      //   tooltip: {
+      //     trigger: 'axis',
+      //     axisPointer: {
+      //       type: 'shadow'
+      //     }
       //   },
-      //   yAxis: {
-      //     type: "value",
+      //   legend: {},
+      //   grid: {
+      //     left: '3%',
+      //     right: '4%',
+      //     bottom: '3%',
+      //     containLabel: true
       //   },
+      //   xAxis: [
+      //     {
+      //       type: 'category',
+      //       data: ['china', 'usa', 'canada', 'germany', 'britain', 'russia', 'france']
+      //     }
+      //   ],
+      //   yAxis: [
+      //     {
+      //       type: 'value'
+      //     }
+      //   ],
       //   series: [
       //     {
-      //       data: [120, 200, 150, 80, 70, 110, 130],
-      //       type: "bar",
-      //       showBackground: true,
-      //       backgroundStyle: {
-      //         color: "rgba(180, 180, 180, 0.2)",
+      //       name: 'Direct',
+      //       type: 'bar',
+      //       emphasis: {
+      //         focus: 'series'
       //       },
+      //       data: [320, 332, 301, 334, 390, 330, 320]
       //     },
-      //   ],
+      //     {
+      //       name: 'Email',
+      //       type: 'bar',
+      //       stack: 'Ad',
+      //       emphasis: {
+      //         focus: 'series'
+      //       },
+      //       data: [120, 132, 101, 134, 90, 230, 210]
+      //     },
+      //     {
+      //       name: 'Union Ads',
+      //       type: 'bar',
+      //       stack: 'Ad',
+      //       emphasis: {
+      //         focus: 'series'
+      //       },
+      //       data: [220, 182, 191, 234, 290, 330, 310]
+      //     },
+      //     {
+      //       name: 'Video Ads',
+      //       type: 'bar',
+      //       stack: 'Ad',
+      //       emphasis: {
+      //         focus: 'series'
+      //       },
+      //       data: [150, 232, 201, 154, 190, 330, 410]
+      //     },
+      //     {
+      //       name: 'Search Engine',
+      //       type: 'bar',
+      //       data: [862, 1018, 964, 1026, 1679, 1600, 1570],
+      //       emphasis: {
+      //         focus: 'series'
+      //       },
+      //       markLine: {
+      //         lineStyle: {
+      //           type: 'dashed'
+      //         },
+      //         data: [[{type: 'min'}, {type: 'max'}]]
+      //       }
+      //     },
+      //     {
+      //       name: 'Baidu',
+      //       type: 'bar',
+      //       barWidth: 5,
+      //       stack: 'Search Engine',
+      //       emphasis: {
+      //         focus: 'series'
+      //       },
+      //       data: [620, 732, 701, 734, 1090, 1130, 1120]
+      //     },
+      //     {
+      //       name: 'Google',
+      //       type: 'bar',
+      //       stack: 'Search Engine',
+      //       emphasis: {
+      //         focus: 'series'
+      //       },
+      //       data: [120, 132, 101, 134, 290, 230, 220]
+      //     },
+      //     {
+      //       name: 'Bing',
+      //       type: 'bar',
+      //       stack: 'Search Engine',
+      //       emphasis: {
+      //         focus: 'series'
+      //       },
+      //       data: [60, 72, 71, 74, 190, 130, 110]
+      //     },
+      //     {
+      //       name: 'Others',
+      //       type: 'bar',
+      //       stack: 'Search Engine',
+      //       emphasis: {
+      //         focus: 'series'
+      //       },
+      //       data: [62, 82, 91, 84, 109, 110, 120]
+      //     }
+      //   ]
       // };
+      let option = {
+        xAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [
+          {
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: "bar",
+            showBackground: true,
+            backgroundStyle: {
+              color: "rgba(180, 180, 180, 0.2)",
+            },
+          },
+        ],
+      };
       myChart.setOption(option);
       // myChart1.setOption(option1);
     },
