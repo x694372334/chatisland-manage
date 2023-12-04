0c88cc6609cda4b216d387317c9afb43a6e95341
jinmingyu <jinmingyu>
2023-12-04 08:16:37 +0800

    chatter基础数据 图形提交 前端画图提交

--- src/views/system/graphical/graphical.vue
+++ src/views/system/graphical/graphical.vue
@@ -3,8 +3,31 @@
     <hr>
     <h3 style="color: black;text-align: center">基础App数据</h3>
     <hr>
-    <el-row style="margin-top:30px">
-      <el-col :span="12" style="text-align: center">
+
+<!--    <el-row>-->
+<!--      <el-card class="box-card">-->
+<!--        <div slot="header" class="clearfix">-->
+<!--          <el-row style="text-align: center;font-size:24px; font-weight: bold;">profile查看转化</el-row>-->
+<!--          <el-row style="text-align: center;margin: 30px 0;">-->
+<!--            <el-date-picker-->
+<!--              v-model="dataRange"-->
+<!--              type="dataRange"-->
+<!--              range-separator="至"-->
+<!--              start-placeholder="开始日期"-->
+<!--              end-placeholder="结束日期"-->
+<!--              @blur="">-->
+<!--            </el-date-picker>-->
+
+<!--            <el-button @click="searchConsumeData">查询</el-button>-->
+<!--          </el-row>-->
+<!--        </div>-->
+<!--        <div class="home">-->
+<!--          <div class="barChart" ref="barChart"></div>-->
+<!--        </div>-->
+<!--      </el-card>-->
+<!--    </el-row>-->
+
+    <el-row style="text-align: center">
         <el-date-picker
           v-model="dataRange"
           type="daterange"
@@ -14,7 +37,6 @@
           @blur=""
           @change="handleChange">
         </el-date-picker>
-      </el-col>
     </el-row>
     <div class="home" style="margin-top:30px">
       <div class="barChart" ref="barChart" ></div>
@@ -23,6 +45,8 @@
 </template>
 
 <script>
+import {basicsAppData} from "@/api/system/graphical";
+
 export default {
   mounted() {
     // 初始化 echarts
@@ -35,28 +59,93 @@ export default {
       // 版本号
       version: "4.8.0",
       dataRange:[],
-      reportRange:[1, 6, 12, 18, 34, 57, 64],
+      reportRangeDNU:[0, 0, 0, 0, 0, 0, 0],
+      reportRangeDNUTest:[],
+      reportRangeWNU:[0, 0, 0, 0, 0, 0, 0],
+      reportRangeWNUTest:[],
       legendRange:['DNU', 'WNU', 'MNU', 'DAU', 'WAU','MAU','次留','3留','7留','14留','30留'],
-      xAxisRange:['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      xAxisRange:['12-03'],
+      xAxisRangeTest:[],
+      systemDate:{},
+      reportRangeMNU:[0, 0, 0, 0, 0, 0, 0],
+      reportRangeMNUTest:[],
+      reportRangeDAU:[0, 0, 0, 0, 0, 0, 0],
+      reportRangeDAUTest:[],
+      reportRangeWAU:[0, 0, 0, 0, 0, 0, 0],
+      reportRangeWAUTest:[],
+      reportRangeMAU:[0, 0, 0, 0, 0, 0, 0],
+      reportRangeMAUTest:[],
+      reportRangeNextLeave:[0, 0, 0, 0, 0, 0, 0],
+      reportRangeNextLeaveTest:[],
+      reportRangeThreeLeave:[0, 0, 0, 0, 0, 0, 0],
+      reportRangeThreeLeaveTest:[],
+      reportRangeSevenLeave:[0, 0, 0, 0, 0, 0, 0],
+      reportRangeSevenLeaveTest:[],
+      reportRangeFourteenLeave:[0, 0, 0, 0, 0, 0, 0],
+      reportRangeFourteenLeaveTest:[],
+      reportRangeThirtyLeave:[0, 0, 0, 0, 0, 0, 0],
+      reportRangeThirtyLeaveLeaveTest:[],
     };
   },
   methods: {
     goTarget(href) {
       window.open(href, "_blank");
     },
+    searchConsumeData() {
+      let startDate = this.consumeDateRange[0]
+      let endDate = this.consumeDateRange[1]
+      let data = {
+        startDate: startDate,
+        endDate: endDate
+      }
+      this.handleChange(data)
+    },
     handleChange() {
-      console.log(this.dataRange)
-      console.log(this.dataRange[0])
-      console.log(this.dataRange[1])
-      this.reportRange[0]=123;
-      this.reportRange[1]=244;
-      console.log(this.reportRange[0])
-      console.log(this.reportRange)
-      console.log(this.legendRange)
-      this.legendRange[0]="aaa";
-      this.xAxisRange[0]="data";
-      console.log(this.legendRange)
-      this.initBarChart();
+
+      let startDate = this.dataRange[0]
+      let endDate = this.dataRange[1]
+      let data = {
+        startDate: startDate,
+        endDate: endDate
+      }
+
+      basicsAppData(data).then(response =>{
+        this.xAxisRange = [];
+
+          if(response.data.length > 7){
+            this.$message.error("不可超过七天,否则无法展示！")
+            return;
+          }
+
+        for (var i = 0; i < response.data.length; i++) {
+          this.systemDate = response.data[i];
+          this.xAxisRangeTest.push(Object.keys(response.data[i]));
+          this.reportRangeDNUTest.push(this.systemDate[Object.keys(this.systemDate)].dnu)
+          this.reportRangeWNUTest.push(this.systemDate[Object.keys(this.systemDate)].wnu)
+          this.reportRangeMNUTest.push(this.systemDate[Object.keys(this.systemDate)].mnu)
+          this.reportRangeDAUTest.push(this.systemDate[Object.keys(this.systemDate)].dau)
+          this.reportRangeWAUTest.push(this.systemDate[Object.keys(this.systemDate)].wau)
+          this.reportRangeMAUTest.push(this.systemDate[Object.keys(this.systemDate)].mau)
+          this.reportRangeNextLeaveTest.push(this.systemDate[Object.keys(this.systemDate)].nextLeave)
+          this.reportRangeThreeLeaveTest.push(this.systemDate[Object.keys(this.systemDate)].threeLeave)
+          this.reportRangeSevenLeaveTest.push(this.systemDate[Object.keys(this.systemDate)].sevenLeave)
+          this.reportRangeFourteenLeaveTest.push(this.systemDate[Object.keys(this.systemDate)].fourteenLeave)
+          this.reportRangeThirtyLeaveLeaveTest.push(this.systemDate[Object.keys(this.systemDate)].thirtyLeave)
+        }
+        this.xAxisRange = this.xAxisRangeTest;
+        this.reportRangeDNU = this.reportRangeDNUTest;
+        this.reportRangeWNU = this.reportRangeWNUTest;
+        this.reportRangeMNU = this.reportRangeMNUTest;
+        this.reportRangeDAU = this.reportRangeDAUTest;
+        this.reportRangeWAU = this.reportRangeWAUTest;
+        this.reportRangeMAU = this.reportRangeMAUTest;
+        this.reportRangeNextLeave = this.reportRangeNextLeaveTest;
+        this.reportRangeThreeLeave = this.reportRangeThreeLeaveTest;
+        this.reportRangeSevenLeave = this.reportRangeSevenLeaveTest;
+        this.reportRangeFourteenLeave = this.reportRangeFourteenLeaveTest;
+        this.reportRangeThirtyLeave = this.reportRangeThirtyLeaveLeaveTest;
+        this.initBarChart();
+      })
     },
     initBarChart() {
       // 通过 $ref 进行挂载
@@ -95,67 +184,67 @@ export default {
             name: 'DNU',
             type: 'line',
             stack: 'Total',
-            data: this.reportRange
+            data: this.reportRangeDNU
           },
           {
             name: 'WNU',
             type: 'line',
             stack: 'Total',
-            data: [1, 10, 50, 60, 75, 80, 105]
+            data: this.reportRangeWNU
           },
           {
             name: 'MNU',
             type: 'line',
             stack: 'Total',
-            data: [5, 10, 15, 20, 40, 50, 65]
+            data: this.reportRangeMNU
           },
           {
             name: 'DAU',
             type: 'line',
             stack: 'Total',
-            data: [6, 12, 22, 24, 35, 42, 48]
+            data: this.reportRangeDAU
           },
           {
             name: 'WAU',
             type: 'line',
             stack: 'Total',
-            data: [1, 6, 12, 18, 34, 57, 64]
+            data: this.reportRangeWAU
           },
           {
             name: 'MAU',
             type: 'line',
             stack: 'Total',
-            data: [6, 15, 24, 32, 46, 58, 66]
+            data: this.reportRangeMAU
           },
           {
             name: '次留',
             type: 'line',
             stack: 'Total',
-            data: [1, 5, 6, 6, 7, 10, 12]
+            data: this.reportRangeNextLeave
           },
           {
             name: '3留',
             type: 'line',
             stack: 'Total',
-            data: [0, 0, 0, 1, 2, 3, 4]
+            data: this.reportRangeThreeLeave
           },
           {
             name: '7留',
             type: 'line',
             stack: 'Total',
-            data: [0, 0, 0, 0, 1, 2, 2]
+            data: this.reportRangeSevenLeave
           },
           {
             name: '14留',
             type: 'line',
             stack: 'Total',
-            data: [0, 0, 0, 0, 0, 1, 1]
+            data: this.reportRangeFourteenLeave
           },
           {
             name: '30留',
             type: 'line',
             stack: 'Total',
-            data: [0, 0, 0, 0, 0, 0, 1]
+            data: this.reportRangeThirtyLeave
           }
         ]
       };
--- src/views/system/graphical/graphicalFive.vue
+++ src/views/system/graphical/graphicalFive.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
     <hr>
-    <h3 style="color: black;text-align: left">人设维度 - 建联转化</h3>
+    <h3 style="color: black;text-align: center">人设维度 - 建联转化</h3>
     <hr>
+    <el-row style="text-align: center">
+      <el-date-picker
+        v-model="dataRange"
+        type="daterange"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        @blur=""
+        @change="handleChange(1)">
+      </el-date-picker>
+    </el-row>
   <div class="home">
     <div class="barChart" ref="barChart"></div>
   </div>
@@ -22,9 +33,13 @@ export default {
     return {
       // 版本号
       version: "4.8.0",
+      dataRange:[],
     };
   },
   methods: {
+    handleChange() {
+
+    },
     goTarget(href) {
       window.open(href, "_blank");
 
@@ -56,7 +71,7 @@ export default {
         xAxis: [
           {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            data: ['user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7'],
             axisPointer: {
               type: 'shadow'
             }
@@ -90,11 +105,11 @@ export default {
             type: 'bar',
             tooltip: {
               valueFormatter: function (value) {
-                return value + ' ml';
+                return value + '';
               }
             },
             data: [
-              2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
+              0, 0, 0, 0, 0, 0, 0
             ]
           },
           {
@@ -103,10 +118,10 @@ export default {
             yAxisIndex: 1,
             tooltip: {
               valueFormatter: function (value) {
-                return value + ' °C';
+                return value + '%';
               }
             },
-            data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+            data: [0, 0, 0, 0, 0, 0, 0]
           }
         ]
       };
