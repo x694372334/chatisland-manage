8d54972dcd9c45472cd6df362d5e521bcff6e371
刘海舟 <425805166@qq.com>
2024-01-02 20:55:03 +0800

    修改薪资管理bug

--- src/views/system/chatterSalary/index.vue
+++ src/views/system/chatterSalary/index.vue
@@ -74,7 +74,7 @@
           <el-table-column label="Violation Diamond" align="center" key="violationDiamond" prop="violationDiamond"/>
           <el-table-column label="The Final Diamond Obtained" align="center" >
             <template slot-scope="scope">
-              <span>{{scope.row.giftDiamond+scope.row.unlockDiamond+scope.row.memberPriceSum-scope.row.violationDiamond}}</span>
+              <span>{{scope.row.giftDiamond+scope.row.unlockDiamond+parseFloat(scope.row.memberPriceSum)-scope.row.violationDiamond}}</span>
             </template>
           </el-table-column>
           <el-table-column
